0a7be4f8f401216504a2dc412e2bec78280844a2
HAJIME Fukuna <f97one@gmail.com>
2020-05-06 12:24:20 +0000

    標準機能を使うのに引数なしコンストラクタが必要なので、data class形式を取りやめ。

--- src/main/kotlin/net/formula97/batchapps/linebreakawarecsvimporter/domain/CsvUser.kt
+++ src/main/kotlin/net/formula97/batchapps/linebreakawarecsvimporter/domain/CsvUser.kt
@@ -2,10 +2,15 @@ package net.formula97.batchapps.linebreakawarecsvimporter.domain
 
 import com.opencsv.bean.CsvBindByPosition
 
-data class CsvUser(
-        @CsvBindByPosition(position = 1, required = true)
-        var username: String,
-        @CsvBindByPosition(position = 2, required = false)
-        var description: String?
-) {
+class CsvUser() {
+    @CsvBindByPosition(position = 1, required = true)
+    var username: String = ""
+
+    @CsvBindByPosition(position = 2, required = false)
+    var description: String? = null
+
+    constructor(username: String, description: String?) : this() {
+        this.username = username
+        this.description = description
+    }
 }
