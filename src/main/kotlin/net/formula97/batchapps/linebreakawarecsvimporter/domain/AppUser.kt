1e17cf83ae5ddffe7cd2447a7be170cebce3fd62
HAJIME Fukuna <f97one@gmail.com>
2020-05-06 04:33:34 +0000

    中身をDDLをもとに作成。

--- src/main/kotlin/net/formula97/batchapps/linebreakawarecsvimporter/domain/AppUser.kt
+++ src/main/kotlin/net/formula97/batchapps/linebreakawarecsvimporter/domain/AppUser.kt
@@ -1,4 +1,15 @@
 package net.formula97.batchapps.linebreakawarecsvimporter.domain
 
+import javax.persistence.*
+
+@Entity(name = "app_user")
 class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    var userId: Int? = null
+    @Column(name = "username", nullable = false, length = 32)
+    var username: String = ""
+    @Column(name = "description", length = 1024)
+    var description: String? = null
 }
